c6c1f00aebc1b058dfaa2861e23e3d34f9683e4f
Diane <dianeanhduong@gmail.com>
2025-04-02 00:56:01 +0200

    first commit

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>Coming soon</h1>
+    </div>
+  );
+}
